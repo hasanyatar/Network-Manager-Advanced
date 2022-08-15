01cf5baa5d49f7fff68e36562891b35cb81c91f4
hsnytr99 <hsnytr99@gmail.com>
2022-08-15 16:36:49 +0300

    Add a simple test

---
+++ app/test/services/products_service_test.dart
@@ -0,0 +1,24 @@
+import 'package:app/app/domain/usecase/products_usecase.dart';
+import 'package:app/app/injector.dart';
+import 'package:app/flavors/flavors.dart';
+import 'package:dartz/dartz.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  setUp(() async {
+    await initializeDepedencies();
+  });
+  EnvConfig devConfig = EnvConfig(
+    appName: 'dev app',
+    baseUrl: 'https://dummyjson.com',
+  );
+  BuildConfig.instance.initialize(envType: Environment.development, envConfig: devConfig);
+
+  group('products', () {
+    test("should return a list of products", () async {
+      final data = injector.get<ProductUseCase>().call(null);
+      expect(Right(data), isNotNull);
+    });
+  });
+}
